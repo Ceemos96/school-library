a6b4d1ff1f534d15756cd823b0f3a1230c96ea2c
Suleiman Olaide <olaide.suleiman96@gmail.com>
2023-01-05 17:50:55 -0800

    update student class

--- student.rb
+++ student.rb
@@ -4,9 +4,14 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    add_classroom(@classroom)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
